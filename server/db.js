27c7d0e5d639efd902c4d968f94d3bd7e4c9714e
Harjeet Singh <harjeetss@harjeet.com>
2021-03-11 00:31:20 +0530

    Setup the Backend with Node.js

---
+++ server/db.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_DATABASE_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+require("./db");
+
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+app.get("/", (req, res) => {
+  res.send("<h2>This is from index.js file</h2>");
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
