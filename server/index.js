b8959c79e8c6bdd03581a05245e5e9cb26953dbe
Harjeet Singh <harjeetss@harjeet.com>
2021-03-11 00:45:38 +0530

    Call REST APIs from a React App

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 require("dotenv").config();
+const userRouter = require("./routers/user");
 require("./db");
 
 const app = express();
 const PORT = process.env.PORT || 3030;
+
+app.use(express.json());
+app.use(userRouter);
 
 app.get("/", (req, res) => {
   res.send("<h2>This is from index.js file</h2>");
